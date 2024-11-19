47cbd4d1684154ad62b62bfedd14a4cf7e35e945
Zholtikov-A <tigerpard@yandex.ru>
2024-11-19 12:02:44 +0300

    Initial commit

---
+++ src/main/java/com/zholtikov/Main.java
@@ -0,0 +1,13 @@
+package com.zholtikov;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "Hello ";
+        String b = "world";
+
+        String result = a+b;
+
+        System.out.println("Hello world!");
+        System.out.println(result);
+    }
+}
